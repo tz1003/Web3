53caf5494bf80fa8db415a8c1e4ff43d603abce7
tz1003 <ucaptzh@ucl.ac.uk>
2022-05-21 07:10:59 +0800

    adding github command

---
+++ github_usage_note/github_usage.sh
@@ -0,0 +1,7 @@
+mkdir "folder_name" # make folder
+git init #initial folder
+git add file #adding file
+#or git add *
+git commit -m "message" # commit the adding
+git remote add origin git@github.com:tz1003/Web3.git
+git push -u origin main 
